88bed5514201c863aa75aec7fee7754c1dbd51a4
shalini-d <39051507+shalini-d@users.noreply.github.com>
2019-02-06 10:15:28 +0530

    Update compile-build-push.sh

--- compile-build-push.sh
+++ compile-build-push.sh
@@ -9,7 +9,6 @@ if ["$1" = "atom-agent"]	#Module 1
 then
 	cd /agent-installer
 	mvn install -P release
-	mv  target/my-app-1.0-SNAPSHOT.jar  /var/www/html/jars/
 	cd docker/dockerdeploy/docker/src
 	cp -f /var/www/html/jars/naas-agent-installer.jar .
 	docker build -t $HOST/$NAME/atom-agent:$2 -f Dockerfile.agent .
@@ -18,7 +17,6 @@ elif ["$1" = "atom-core"]	#Module 2
 then
 	cd /installer
 	mvn installer -P release-agentserver
-	mv target/naas-server-installer.jar /var/www/html/jars/
 	cd docker/dockerdeploy/docker/src
 	cp -f /var/ww/html/jars/naas-server-installer.jar .
 	docker build -t $HOST/$NAME/atom-core:$2 -f Dockerfile.core .
@@ -39,7 +37,6 @@ elif ["$1" = "atom-scheduler"]	#Module 4
 then
 	cd /microservices/scheduler
 	mvn clean install -DskipTests
-	mv target/scheduler-1.0.0-SNAPSHOT.jar /var/www/html/jars/
 	cd docker/dockerdeploy/docker/src
 	cp -f /var/www/html/jars/scheduler-1.0.0-SNAPSHOT.jar .
 	docker build -t $HOST/$NAME/atom-scheduler:0.1 -f Dockerfile.scheduler .
@@ -48,7 +45,6 @@ elif ["$1" = "atom-telemetry-engine"]	#Module 5
 then
 	cd microservices/telemetry
 	mvn clean install -DskipTests
-	mv target/telemetry-engine-1.0.jar /var/www/html/jars/
 	cd microservices/telemetry/src/main/resources/
 	cp -f /var/www/html/jars/telemetry-engine-1.0.jar .
 	docker build -t $HOST/$NAME/atom-telemetry-engine:0.1 -f Dockerfile .
@@ -57,7 +53,6 @@ elif ["$1" = "atom-pnp-server"]	#Module 6
 then
 	cd /microservices/pnpserver
 	mvn clean install -DskipTests
-	mv target/pnp-server-*.jar /var/www/html/jars/
 	cd microservices/pnpserver/src/main/resources/
 	cp -f /var/www/html/jars/pnp-server-*.jar .
 	docker build -t $HOST/$NAME/atom-pnp-server:0.1 -f Dockerfile .
@@ -66,7 +61,6 @@ elif ["$1" = "atom-workflow-engine"]	#Module 7
 then
 	cd microservices/workflow
 	mvn clean install -DskipTests
-	mv target/atom-wrkflow-*.jar /var/www/html/jars/
 	cd microservices/workflow/src/main/resources/
 	cp -f /var/www/html/jars/atom-workflow-*.jar .
 	docker build -t $HOST/$NAME/atom-workflow-engine:0.1 -f Dockerfile .
@@ -75,7 +69,6 @@ elif ["$1" = "atom-telemetry-exporter"]	#Module 8
 then
 	cd microservices/telemetry-kafka-exporter
 	mvn clean install -DskipTests
-	mv target/kafka-exporter*.jar /var/www/html/jars
 	cd microservices/telemetry-kafka-exporter/src/main/resources/
 	cp /var/www/html/jars/kafka-exporter*.jar .
 	docker build -t $HOST/$NAME/atom-telemetry-exporter:0.1 -f Dockerfile .
